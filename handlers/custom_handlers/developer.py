fb276a0192f74e441139ab881278dc873500e05b
Dextiles <Dextiles@outlook.com>
2024-03-13 17:51:11 +0300

    About info update

--- handlers/custom_handlers/developer.py
+++ handlers/custom_handlers/developer.py
@@ -11,8 +11,5 @@ def about_me(message: Message):
     Returns None.
     """
     bot.send_message(message.chat.id, '\U0001F464 Разработчик сервиса: Иван Пермяков\n'
-                                      'Fullstack-разработчик, Веб-дизайнер\n(сайты под ключ, '
-                                      'телеграм боты, windows и web приложения)\n'
-                                      'Пишу на C#/C++/Python/SQL\n\n'
                                       'Основной директ, сотрудничество: @Dextiles\n\n',
                      reply_markup=about_developer.get_about_developer_markup())
